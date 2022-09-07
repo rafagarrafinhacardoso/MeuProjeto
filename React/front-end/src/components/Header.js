9a2ab0d7ef8a2900156e6184742f4e4f8424b630
rafagarrafinhacardoso@gmail.com <rafagarrafinhacardoso@gmail.com>
2022-09-07 18:35:46 -0300

    Rotas e menu funcional, em desenvolvimento

---
+++ React/front-end/src/components/Header.js
@@ -0,0 +1,151 @@
+import { AccountCircle, Assignment, BarChart, Dashboard, Layers, People, PhotoCamera, ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, Container, createTheme, CssBaseline, Divider, Drawer, Grid, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, ListSubheader, Menu, MenuItem, Paper, ThemeProvider, Toolbar, Typography } from "@mui/material";
+import React, { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+const barraMenu = [
+    {"text": "Pagina Inicial", "link": "/", "id": 1},
+    {"text": "Segunda Pagina", "link": "about", "id": 2},
+    {"text": "Terceira Pagina", "link": "/", "id": 3},
+    {"text": "Fuck Pagina", "link": "/about", "id": 4}
+];
+
+export default function Header() {
+    let navigate = useNavigate();
+
+    const [open, setOpen] = useState(false);
+
+    const buttonRef = useRef();
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const toggleDrawer = () => {
+        setOpen(!open);
+        // console.log("test");
+        // console.log(open);
+    };
+
+    function clickItemMenu(item) {
+        console.log(item)
+        navigate(item.link, { replace: true });
+    }
+
+    return (
+        <>
+            <AppBar position="absolute" open={open}>
+                <Toolbar
+                    sx={{
+                        pr: '24px', // keep right padding when drawer closed
+                    }}
+                >
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={toggleDrawer}
+                        sx={{
+                            marginRight: '36px',
+                            ...(open && { display: 'none' }),
+                        }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography
+                        component="h1"
+                        variant="h6"
+                        color="inherit"
+                        noWrap
+                        sx={{ flexGrow: 1 }}
+                    >
+                        Dashboard
+                    </Typography>
+                    <IconButton
+                        color="inherit"
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls={anchorEl ? "simple-menu" : undefined}
+                        aria-haspopup="true"
+                        onClick={handleMenu}
+                        ref={buttonRef}
+                    >
+                        <Badge color="secondary">
+                            <AccountCircle />
+                        </Badge>
+                    </IconButton>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={buttonRef.current}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleClose}>Profile</MenuItem>
+                        <MenuItem onClick={handleClose}>My account</MenuItem>
+                    </Menu>
+                </Toolbar>
+            </AppBar>
+            <Drawer open={open} onClose={() => setOpen(false)}>
+                <Toolbar
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'flex-end',
+                        px: [1],
+                    }}
+                >
+                    <IconButton onClick={toggleDrawer}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </Toolbar>
+                <Divider />
+                {/* <List component="nav">
+                    {mainListItems}
+                </List> */}
+                <List>
+                    {barraMenu.map((item, index) => (
+                        <ListItem key={item.id} disablePadding sx={{ display: 'block' }}>
+                            <ListItemButton
+                                sx={{
+                                    minHeight: 48,
+                                    justifyContent: open ? 'initial' : 'center',
+                                    px: 2.5,
+                                }}
+                                onClick={() => clickItemMenu(item)}
+                            >
+                                <ListItemIcon
+                                    sx={{
+                                        minWidth: 0,
+                                        mr: open ? 3 : 'auto',
+                                        justifyContent: 'center',
+                                    }}
+                                >
+                                    <ArrowForwardIosIcon />
+                                </ListItemIcon>
+                                <ListItemText primary={item.text} sx={{ opacity: open ? 1 : 0 }} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+        </>
+    );
+
+}
--- React/front-end/src/components/Home.js
+++ React/front-end/src/components/Home.js
@@ -1,150 +1,61 @@
-import { Assignment, BarChart, Dashboard, Layers, People, PhotoCamera, ShoppingCart } from "@mui/icons-material";
-import { AppBar, Badge, Box, createTheme, CssBaseline, Divider, Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText, ListSubheader, ThemeProvider, Toolbar, Typography } from "@mui/material";
-import React from "react";
+import { AccountCircle, Assignment, BarChart, Dashboard, Layers, People, PhotoCamera, ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, Container, createTheme, CssBaseline, Divider, Drawer, Grid, IconButton, List, ListItemButton, ListItemIcon, ListItemText, ListSubheader, Menu, MenuItem, Paper, ThemeProvider, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import Header from "./Header";
 // import CameraIcon from '@mui/icons-material/PhotoCamera';
 
 const theme = createTheme();
 
 export default function Home() {
-    const [open, setOpen] = React.useState(true);
+    const [open, setOpen] = useState(false);
+    const [perfil, setPerfil] = useState(false);
+
+    const buttonRef = React.useRef();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+     };
+     const handleClose = () => {
+         setAnchorEl(null);
+     };
 
     const toggleDrawer = () => {
         setOpen(!open);
-        console.log("test");
-        console.log(open);
+        // console.log("test");
+        // console.log(open);
     };
 
     return (
         <ThemeProvider theme={theme}>
             <Box sx={{ display: 'flex' }}>
                 <CssBaseline />
-                <AppBar position="absolute" open={open}>
-                    <Toolbar
-                        sx={{
-                            pr: '24px', // keep right padding when drawer closed
-                        }}
-                    >
-                        <IconButton
-                            edge="start"
-                            color="inherit"
-                            aria-label="open drawer"
-                            onClick={toggleDrawer}
-                            sx={{
-                                marginRight: '36px',
-                                ...(open && { display: 'none' }),
-                            }}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography
-                            component="h1"
-                            variant="h6"
-                            color="inherit"
-                            noWrap
-                            sx={{ flexGrow: 1 }}
-                        >
-                            Dashboard
-                        </Typography>
-                        <IconButton color="inherit">
-                            <Badge badgeContent={4} color="secondary">
-                                <NotificationsIcon />
-                            </Badge>
-                        </IconButton>
-                    </Toolbar>
-                </AppBar>
-                <Drawer open={open}  onClose={() => setOpen(false)}>
-                    <Toolbar
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'flex-end',
-                            px: [1],
-                        }}
-                    >
-                        <IconButton onClick={toggleDrawer}>
-                            <ChevronLeftIcon />
-                        </IconButton>
-                    </Toolbar>
-                    <Divider />
-                    <List component="nav">
-                        {mainListItems}
-                        <Divider sx={{ my: 1 }} />
-                        {secondaryListItems}
-                    </List>
-                </Drawer>
-                <main>
-                    <h2>Welcome to the homepage!</h2>
-                    <p>You can do this, I believe in you.</p>
-                </main>
-                <nav>
-                    <Link to="/about">About</Link>
-                </nav>
+                <Header />
+                <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                    <Grid container spacing={3} rowSpacing={1}  >
+                        <Grid item xs={12}>
+                            <Paper sx={{ p: 3, display: 'flex', flexDirection: 'column' }}>
+                                <main>
+                                    <h2>Welcome to the homepage!</h2>
+                                    <p>You can do this, I believe in you.</p>
+                                </main>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                                <nav>
+                                    <Link to="/about">About</Link>
+                                </nav>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Container>
+
             </Box>
         </ThemeProvider>
     );
 }
-
-const mainListItems = (
-    <React.Fragment>
-        <ListItemButton>
-            <ListItemIcon>
-                <Dashboard />
-            </ListItemIcon>
-            <ListItemText primary="Dashboard" />
-        </ListItemButton>
-        <ListItemButton>
-            <ListItemIcon>
-                <ShoppingCart />
-            </ListItemIcon>
-            <ListItemText primary="Orders" />
-        </ListItemButton>
-        <ListItemButton>
-            <ListItemIcon>
-                <People />
-            </ListItemIcon>
-            <ListItemText primary="Customers" />
-        </ListItemButton>
-        <ListItemButton>
-            <ListItemIcon>
-                <BarChart />
-            </ListItemIcon>
-            <ListItemText primary="Reports" />
-        </ListItemButton>
-        <ListItemButton>
-            <ListItemIcon>
-                <Layers />
-            </ListItemIcon>
-            <ListItemText primary="Integrations" />
-        </ListItemButton>
-    </React.Fragment>
-);
-
-const secondaryListItems = (
-    <React.Fragment>
-        <ListSubheader component="div" inset>
-            Saved reports
-        </ListSubheader>
-        <ListItemButton>
-            <ListItemIcon>
-                <Assignment />
-            </ListItemIcon>
-            <ListItemText primary="Current month" />
-        </ListItemButton>
-        <ListItemButton>
-            <ListItemIcon>
-                <Assignment />
-            </ListItemIcon>
-            <ListItemText primary="Last quarter" />
-        </ListItemButton>
-        <ListItemButton>
-            <ListItemIcon>
-                <Assignment />
-            </ListItemIcon>
-            <ListItemText primary="Year-end sale" />
-        </ListItemButton>
-    </React.Fragment>
-);
